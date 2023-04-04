afb19959f70973c37fd45290e42fc65e20e548b5
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-04-04 12:47:32 -0600

    About us, Styles and meta attributes

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,7 +1,59 @@
+import imagen from "../../public/img/nosotros.jpg"
+import styles from "~/styles/nosotros.css"
+
+export function meta(){
+  return [
+    {
+      title: "GuitarLA - Sobre Nosotros"
+    },
+    {
+      description: "Venta de guitarras, blog de música"
+    }
+  ]
+}
+
+export function links(){
+  return [
+    {
+      rel: "stylesheet",
+      href: styles
+    },
+    {
+      rel: "preload",
+      href: imagen, 
+      as: "image"
+    }
+  ]
+}
 
 function Nosotros() {
   return (
-    <div>Desde Nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+
+      <div className="contenido">
+          <img src={imagen} alt="imagen sobre nosotros" />
+
+          <div>
+              <p>
+                  Quisque dui erat, auctor a augue sit amet, iaculis dignissim 
+                  nisi. Nam ac aliquam eros. In at condimentum turpis. Donec 
+                  mollis ornare urna et vestibulum. Nullam sollicitudin risus 
+                  ut odio venenatis fermentum. Pellentesque ut justo tortor. 
+                  stibulum scelerisque massa sit amet sagittis faucibus. 
+              </p>
+
+              <p>
+                  Quisque dui erat, auctor a augue sit amet, iaculis dignissim 
+                  nisi. Nam ac aliquam eros. In at condimentum turpis. Donec 
+                  mollis ornare urna et vestibulum. Nullam sollicitudin risus 
+                  ut odio venenatis fermentum. Pellentesque ut justo tortor. 
+                  stibulum scelerisque massa sit amet sagittis faucibus. 
+              </p>
+
+          </div>
+      </div>
+    </main>
   )
 }
 
