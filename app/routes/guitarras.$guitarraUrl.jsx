dcc5c9945dab113282fa644884b1d3251748e98e
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-04-05 19:03:44 -0600

    Filter guitars by URL, dynamic routing and showing guitar information

---
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -0,0 +1,42 @@
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarra } from "~/models/guitarras.server"
+import styles from "~/styles/guitarras.css"
+
+export function links(){
+    return [
+        {
+            rel: "stylesheet",
+            href: styles
+        }
+    ]
+}
+
+export async function loader({params}){
+
+    const { guitarraUrl } = params;
+
+    const guitarra = await getGuitarra( guitarraUrl );
+
+    return guitarra;
+    
+}
+
+function Guitarra() {
+
+    const guitarra = useLoaderData();
+    const { nombre, descripcion, imagen, precio} = guitarra.data[0].attributes
+
+  return (
+    <main className="contenedor guitarra">
+        <img className="imagen" src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} />
+
+        <div className="contenido">
+            <h3>{nombre}</h3>
+            <p className="texto">{descripcion}</p>
+            <p className="precio">${precio}</p>
+        </div>
+    </main>
+  )
+}
+
+export default Guitarra
