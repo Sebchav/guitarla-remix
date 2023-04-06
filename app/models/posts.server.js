93d91b5db310c45d933d5128d02281fd32ce54cc
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-04-06 12:42:22 -0600

    Dynamic routing for the blog and ending the blog

--- app/models/posts.server.js
+++ app/models/posts.server.js
@@ -3,3 +3,8 @@ export async function getPosts(){
     const resultado = await respuesta.json();
     return resultado;
 }
+
+export async function getPost(url){
+    const respuesta = await fetch(`${process.env.API_URL}/posts?filters[url]=${url}&populate=imagen`);
+    return await respuesta.json();
+}
--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -3,6 +3,13 @@ import { getPosts } from "~/models/posts.server"
 import Post from "../components/post";
 import styles from "~/styles/blog.css"
 
+export function meta(){
+  return [
+      { title: "GuitarLA - Nuestro Blog"},
+      { descripcion: "GuitarLA, Blog de música y venta de guitarras"}
+  ]
+}
+
 export function links(){
   return [
     {
