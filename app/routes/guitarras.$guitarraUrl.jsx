36d29e5c511305e33356fb8df06c2bccebaf4af7
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-04-09 18:40:48 -0600

    Add to cart, validating and Remix state

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "@remix-run/react";
+import { useState } from "react"
 import { getGuitarra } from "~/models/guitarras.server"
 import styles from "~/styles/guitarras.css"
 
@@ -46,8 +47,28 @@ export function links(){
 
 function Guitarra() {
 
+    const [cantidad, setCantidad] = useState(0);
     const guitarra = useLoaderData();
     const { nombre, descripcion, imagen, precio} = guitarra.data[0].attributes
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        if(cantidad < 1){
+            alert("Debes seleccionar una cantidad")
+            return;
+        }
+
+        const guitarraSeleccionada = {
+            id: guitarra.data[0].id,
+            imagen: imagen.data.attributes.url,
+            nombre,
+            precio,
+            cantidad
+        }
+
+        console.log(guitarraSeleccionada)
+    }
 
   return (
     <main className="contenedor guitarra">
@@ -57,6 +78,27 @@ function Guitarra() {
             <h3>{nombre}</h3>
             <p className="texto">{descripcion}</p>
             <p className="precio">${precio}</p>
+
+            <form onSubmit={handleSubmit} className="formulario">
+                <label htmlFor="cantidad">Cantidad</label>
+
+                <select 
+                onChange={e => setCantidad(parseInt(e.target.value))}
+                id="cantidad">
+                    <option value="0">-- Seleccione --</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+
+                </select>
+
+                <input 
+                    type="submit"
+                    value="Agregar al Carrito"
+                />
+            </form>
         </div>
     </main>
   )
